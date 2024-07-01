7238aed418fd91048b4db86ae350020c4419f0ad
kazetatsu <kazemaru.tatsunoshin@gmail.com>
2024-07-01 12:44:57 +0900

    change pin assign for seeed xiao Rp2040

--- src/mic.h
+++ src/mic.h
@@ -3,13 +3,18 @@
 
 #include "pico/types.h"
 
-#define PIN_MOSI  16 // to   Pico from MCP3008
-#define PIN_CS    17
-#define PIN_SCLK  18
-#define PIN_MISO  19 // from Pico to   MCP3008
+// #define PIN_MOSI  16 // to   Pico from MCP3008
+// #define PIN_CS    17
+// #define PIN_SCLK  18
+// #define PIN_MISO  19 // from Pico to   MCP3008
+
+#define PIN_MOSI  3 // to   xiao from MCP3008
+#define PIN_CS    1
+#define PIN_SCLK  2
+#define PIN_MISO  4 // from xiao to   MCP3008
 
 #define CH_NUM 6
-#define SOUND_DEPTH 2
+#define SOUND_DEPTH 64
 
 void mic_init();
 void mic_read(uint16_t data[][CH_NUM], uint8_t depth);
